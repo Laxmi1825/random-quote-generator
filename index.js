caa336ad988a5f9cf58d82a26b58e8268785c8a8
Laxmi1825 <laxmigaddala98@gmail.com>
2024-08-10 13:26:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const  buttonEl = document.getElementById("button");
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+
+const apiUrl ="https://api.quotable.io/random";
+
+ async function getquote(){
+    
+    try {
+        const response =   await fetch(apiUrl)
+        const data = await response.json()
+        const quoteContent = data.content;
+        const quoteAuthor = data.author;
+        quoteEl.innerText = quoteContent; 
+        authorEl.innerText = "~" +quoteAuthor;
+        console.log(data);   
+    } catch (error) {
+        console.log(error);
+        quoteEl.innerText = "An error happened please try again";
+        authorEl.innerText = "try again";
+        
+        
+    }
+    const response =   await fetch(apiUrl)
+    const data = await response.json()
+    const quoteContent = data.content;
+    const quoteAuthor = data.author;
+    quoteEl.innerText = quoteContent; 
+    authorEl.innerText = "~" +quoteAuthor;
+    console.log(data);
+    
+    
+}
+
+
+
+buttonEl.addEventListener("click", getquote)
